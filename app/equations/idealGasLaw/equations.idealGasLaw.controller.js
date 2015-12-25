536c306a1d8c49f81f8bb97a8d5f61f121f8e6a5
Andreas <aw6018a@student.american.edu>
2015-12-25 11:46:22 -0500

    added MathJax loading to all equation controllers to prevent having to refresh to see the typescript

--- app/equations/idealGasLaw/equations.idealGasLaw.controller.js
+++ app/equations/idealGasLaw/equations.idealGasLaw.controller.js
@@ -2,5 +2,7 @@
  * Created by andre on 22-Dec-15.
  */
 angular.module('cheRefAppApp').controller('IdealGasLawCtrl', ['$scope', function($scope) {
+  MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+
   $scope.gasLawTest = 'Test Gas Law Controller';
 }]);
--- app/equations/relativity/equations.relativity.controller.js
+++ app/equations/relativity/equations.relativity.controller.js
@@ -2,5 +2,7 @@
  * Created by andre on 22-Dec-15.
  */
 angular.module('cheRefAppApp').controller('RelativityCtrl', ['$scope', function($scope) {
+  MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+
   $scope.relativityText = 'Relativity';
 }]);
